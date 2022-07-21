0c6d771a021fb76c4b2faf7ed94c4d4836e456c9
Dutzuddd <vladsfranciog@gmail.com>
2022-07-21 10:30:33 +0300

    Definit Controllers&Servicii

--- src/main/java/com/ibm/ro/tm/apprenticeship/poll/metter/service/AnswerService.java
+++ src/main/java/com/ibm/ro/tm/apprenticeship/poll/metter/service/AnswerService.java
@@ -3,10 +3,53 @@
  */
 package com.ibm.ro.tm.apprenticeship.poll.metter.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ibm.ro.tm.apprenticeship.poll.metter.entity.Answer;
+import com.ibm.ro.tm.apprenticeship.poll.metter.repository.AnswerRepository;
+
 /**
  * @author vlads
  *
  */
+@Service
 public class AnswerService {
 
+	private final AnswerRepository answerRepository;
+	
+	@Autowired
+	public AnswerService(AnswerRepository answerRepository) {
+		this.answerRepository = answerRepository;
+	}
+
+	/**
+	 * @return the answerRepositoy
+	 */
+	public AnswerRepository getAnswerRepositoy() {
+		return answerRepository;
+	}
+	
+	public Answer addAnswer(Answer answer) {
+		return answerRepository.save(answer);
+	}
+	
+	public List<Answer> findAllAnswers(){
+		return answerRepository.findAll();
+	}
+	
+	public Answer findById(Long id) {
+		return answerRepository.getById(id);
+	}
+	
+	public Answer updateAnswer(Answer answer) {
+		return answerRepository.save(answer);
+	}
+	
+	public void deleteAnswer(Long id) {
+		answerRepository.deleteById(id);
+	}
+	
 }
--- src/main/java/com/ibm/ro/tm/apprenticeship/poll/metter/service/PollService.java
+++ src/main/java/com/ibm/ro/tm/apprenticeship/poll/metter/service/PollService.java
@@ -1,5 +1,49 @@
 package com.ibm.ro.tm.apprenticeship.poll.metter.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ibm.ro.tm.apprenticeship.poll.metter.entity.Poll;
+import com.ibm.ro.tm.apprenticeship.poll.metter.repository.PollRepository;
+
+@Service
 public class PollService {
 
+	private final PollRepository pollRepository;
+	
+	@Autowired
+	public PollService(PollRepository pollRepository) {
+		this.pollRepository = pollRepository;
+	}
+
+	/**
+	 * @return the pollRepository
+	 */
+	public PollRepository getPollRepository() {
+		return pollRepository;
+	}
+	
+	public Poll addPoll(Poll poll) {
+		return pollRepository.save(poll);
+	}
+	
+	public List<Poll> findAllPolls(){
+		return pollRepository.findAll();
+	}
+	
+	public Poll updatePoll(Poll poll) {
+		return pollRepository.save(poll);
+	}
+	
+	public Poll findById(Long id) {
+		return pollRepository.getById(id);
+	}
+	
+	public void deletePoll(Long id) {
+		pollRepository.deleteById(id);
+	}
+	
+		
 }
